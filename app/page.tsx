e4ae6c0f18d9e38bd98ef11480aea1b442f43e66
Krish-Panchani <krishpanchani1346@gmail.com>
2024-01-27 22:40:32 +0530

    Remove commented out code

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,6 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
     <div>
       <Content />
-      {/* <Button>Click me</Button> */}
     </div>
 
     </main>
