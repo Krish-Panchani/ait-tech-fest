515704600ccd3daa30e54e391b859c83bb29b8ba
Krish-Panchani <krishpanchani1346@gmail.com>
2024-01-27 22:40:52 +0530

    Add Welcome component

--- components/content.tsx
+++ components/content.tsx
@@ -10,6 +10,7 @@ import { Button } from "@/components/ui/button"
 import { JSX, SVGProps } from "react"
 import img1 from "@/app/assets/img1.jpeg"
 import { Skeleton } from "./ui/skeleton"
+import Welcome from "./welcome"
 
 const scheduleData = [
     { day: 'Day 1 - Events Soon', time: 'Time Date' },
@@ -25,25 +26,7 @@ export default function Component() {
     return (
         <div className="flex flex-col min-h-[100vh]">
             <main className="flex-1">
-                <section className="w-full pt-12 md:pt-24 lg:pt-24 border-y">
-                    <div className="px-4 md:px-6 space-y-10 xl:space-y-16">
-                        <div className="grid max-w-[1300px] mx-auto gap-4 px-4 sm:px-6 md:px-10 md:grid-cols-2 md:gap-16">
-                            <div>
-                                <h1 className="lg:leading-tighter text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl xl:text-[3.4rem] 2xl:text-[3.75rem]">
-                                    Welcome to TechFest 2024
-                                </h1>
-                                <p className="mx-auto max-w-[700px] text-gray-500 md:text-xl dark:text-gray-400">
-                                    Join us for a week of innovation, learning, and fun! Participate in various tech events, workshops,
-                                    and competitions.
-                                </p>
-                            </div>
-                            <div className="flex flex-col items-start space-y-4">
-                                <Skeleton className="w-[500px] h-[300px] rounded-md mx-auto aspect-[2/1]" />
-                                {/* <img alt="TechFest" className="mx-auto aspect-[2/1] overflow-hidden rounded-t-xl object-cover" height="300" src="/img1.jpeg" width="500" /> */}
-                            </div>
-                        </div>
-                    </div>
-                </section>
+                <Welcome />
                 <section className="w-full py-12 md:py-24 lg:py-32">
                     <div className="container space-y-12 px-4 md:px-6">
                         <div className="flex flex-col items-center justify-center space-y-4 text-center">
