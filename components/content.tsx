240581d226c36b419ea0db26a5e336f10b4f6c2f
Krish-Panchani <krishpanchani1346@gmail.com>
2024-01-28 14:38:54 +0530

    Add Venue component to content

--- components/content.tsx
+++ components/content.tsx
@@ -9,14 +9,15 @@ import img1 from "@/app/assets/img1.jpeg"
 import { Skeleton } from "./ui/skeleton"
 import Welcome from "./welcome"
 import Schedule from "./schedule"
+import Venue from "./venue"
 
 
 export default function Component() {
-    return (
-        <div className="flex flex-col min-h-[100vh]">
-            <main className="flex-1">
-                <Welcome />
-                <Schedule />
+  return (
+    <div className="flex flex-col min-h-[100vh]">
+      <main className="flex-1">
+        <Welcome />
+        <Schedule />
         {/* <section className="w-full py-12 md:py-24 lg:py-32">
           <div className="container px-4 md:px-6">
             <div className="grid items-center gap-6 lg:grid-cols-[1fr_500px] lg:gap-12 xl:grid-cols-[1fr_550px]">
@@ -60,37 +61,28 @@ export default function Component() {
             </div>
           </div>
         </section> */}
-            </main>
-            <footer className="flex flex-col gap-2 sm:flex-row pt-3 w-full shrink-0 items-center px-4 md:px-6 border-t">
-                <p className="text-xs text-gray-500 dark:text-gray-400">© 2024 TechFest. All rights reserved.</p>
-                <nav className="sm:ml-auto flex gap-4 sm:gap-6">
-                    <Link className="text-xs hover:underline underline-offset-4" href="#">
-                        Terms of Service
-                    </Link>
-                    <Link className="text-xs hover:underline underline-offset-4" href="#">
-                        Privacy
-                    </Link>
-                </nav>
-            </footer>
-        </div>
-    )
+        <Venue />
+      </main>
+
+    </div>
+  )
 }
 
 function MountainIcon(props: JSX.IntrinsicAttributes & SVGProps<SVGSVGElement>) {
-    return (
-        <svg
-            {...props}
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-        >
-            <path d="m8 3 4 8 5-5 5 15H2L8 3z" />
-        </svg>
-    )
+  return (
+    <svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path d="m8 3 4 8 5-5 5 15H2L8 3z" />
+    </svg>
+  )
 }
