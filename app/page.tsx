098b4f8ff3bcab72166f278cbf49490d573a2267
Krish-Panchani <krishpanchani1346@gmail.com>
2024-01-28 19:16:07 +0530

    Remove unused imports and update component import

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,5 @@
-import Image from "next/image";
-import { Button } from '@nextui-org/button';
 import React from "react";
-import { Navbar } from "@nextui-org/react";
-import Content from "@/components/content";
-import Menu from "@/components/mobileMenu";
-
+import Content from "@/components/homepage";
 
 export default function Home() {
   return (
