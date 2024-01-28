c590c9acde9c451a4e99fe88aff19ba0b232d0b1
Krish-Panchani <krishpanchani1346@gmail.com>
2024-01-28 16:00:18 +0530

    Add ContextMenu component and related UI components

--- app/providers.tsx
+++ app/providers.tsx
@@ -2,11 +2,35 @@
 'use client'
 
 import {NextUIProvider} from '@nextui-org/react'
+import {
+  ContextMenu,
+  ContextMenuContent,
+  ContextMenuItem,
+  ContextMenuLabel,
+  ContextMenuRadioGroup,
+  ContextMenuSeparator,
+  ContextMenuTrigger,
+} from "@/components/ui/context-menu"
+import { Separator } from '@/components/ui/separator'
 
 export function Providers({children}: { children: React.ReactNode }) {
   return (
+    <ContextMenu>
+        <ContextMenuTrigger>
     <NextUIProvider>
       {children}
     </NextUIProvider>
+    </ContextMenuTrigger>
+        <ContextMenuContent>
+          <ContextMenuRadioGroup value="pedro">
+            <ContextMenuLabel inset>😑 Right Click ..??</ContextMenuLabel>
+          </ContextMenuRadioGroup>
+          {/* <ContextMenuSeparator /> */}
+          <Separator className='bg-gray-500' />
+          <ContextMenuItem>💖 Btw Welcome to AIT TechFest</ContextMenuItem>
+          <ContextMenuItem>🥲 Inspect krnese kuch nhi milega</ContextMenuItem>
+        </ContextMenuContent>
+      </ContextMenu>
+    
   )
 }
