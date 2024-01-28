94512a77f9fac89ce98490d1e2354a2b00b012d5
Krish-Panchani <krishpanchani1346@gmail.com>
2024-01-28 12:10:09 +0530

    Add drawer component and update menu items

--- components/header.tsx
+++ components/header.tsx
@@ -6,20 +6,32 @@ import Link from 'next/link'
 import { Navbar, NavbarBrand, NavbarContent, NavbarItem, NavbarMenu, NavbarMenuItem, NavbarMenuToggle } from "@nextui-org/react";
 import { AcmeLogo } from "@/app/assets/AcmeLogo";
 import { Button } from "./ui/button";
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/components/ui/drawer"
 
 export default function Header() {
   // const router = useRouter();
   const pathname = usePathname()
   const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+
   const handleMenuItemClick = () => {
     setIsMenuOpen(false); // Close the menu when a menu item is clicked
+    isMenuOpen == false;
   };
 
   const menuItems = [
     { label: "Home", path: "/", isActive: pathname === "/" },
-    { label: "About", path: "/about", isActive: pathname === "/about" },
+    // { label: "About", path: "/about", isActive: pathname === "/about" },
     { label: "Events", path: "/events", isActive: pathname === "/events" },
-    { label: "Contact", path: "/contact", isActive: pathname === "/contact" }
+    { label: "Schedule", path: "/contact", isActive: pathname === "/contact" }
   ];
 
   return (
@@ -46,10 +58,33 @@ export default function Header() {
       </NavbarContent>
 
       <NavbarContent justify="end">
+        {/* <NavbarItem> 
+          <Button variant="primary">Contact</Button>
+      </NavbarItem> */}
         <NavbarItem>
-          <Button color="default" variant="primary">
-            <Link href="#">Register</Link>
-          </Button>
+          <Drawer>
+            <DrawerTrigger asChild>
+              {/* <Button variant="orange">Details</Button> */}
+              <Button variant="primary">Register</Button>
+            </DrawerTrigger>
+            <DrawerContent>
+              <div className="mx-auto w-full max-w-sm">
+                <DrawerHeader>
+                  <DrawerTitle>Registration</DrawerTitle>
+                  <DrawerDescription>Details</DrawerDescription>
+                  <DrawerDescription>Registration Form will be release soon, Stay Tuned</DrawerDescription>
+                </DrawerHeader>
+
+                <DrawerFooter>
+                  {/* <Button>Submit</Button> */}
+                  <DrawerClose asChild>
+                    <Button variant="outline">Cancel</Button>
+                  </DrawerClose>
+                </DrawerFooter>
+              </div>
+            </DrawerContent>
+          </Drawer>
+
         </NavbarItem>
       </NavbarContent>
 
@@ -69,6 +104,8 @@ export default function Header() {
           </NavbarMenuItem>
         ))}
       </NavbarMenu>
+      {/* Mobile Menu */}
+    
     </Navbar>
   );
 }
