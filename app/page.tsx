cfb9008d826f42709744f5317df8a0528692f7b3
Krish-Panchani <krishpanchani1346@gmail.com>
2024-01-28 12:10:29 +0530

    Fix formatting in page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,18 @@
 import Image from "next/image";
-import {Button} from '@nextui-org/button'; 
+import { Button } from '@nextui-org/button';
 import React from "react";
 import { Navbar } from "@nextui-org/react";
 import Content from "@/components/content";
+import Menu from "@/components/menu";
 
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-    <div>
-      <Content />
-    </div>
+      <div>
+        <Content />
+      </div>
+        
 
     </main>
   );
