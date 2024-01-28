7ca2660e8ce5816202baa4a6ea1547eb4852f7d2
Krish-Panchani <krishpanchani1346@gmail.com>
2024-01-28 12:10:04 +0530

    Add Menu component

---
+++ components/menu.tsx
@@ -0,0 +1,37 @@
+"use client";
+import React from 'react'
+import { usePathname } from 'next/navigation'
+import Link from 'next/link'
+
+
+export default function Menu() {
+    const pathname = usePathname()
+const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+
+const menuItems = [
+    { label: "Home", path: "/", isActive: pathname === "/" },
+    // { label: "About", path: "/about", isActive: pathname === "/about" },
+    { label: "Events", path: "/events", isActive: pathname === "/events" },
+    { label: "Schedule", path: "/#schedule", isActive: pathname === "/#schedule" }
+  ];
+
+    return (
+        <div className="fixed bottom-5 mx-auto flex justify-center sm:hidden">
+              <div className="border border-orange-400 rounded-full backdrop-blur-md px-4 py-4">
+                <ul className="flex">
+                  {menuItems.map((item, index) => (
+                    <li key={`${item.label}-${index}`} className="px-2">
+                      <Link
+                        className={item.isActive ? "font-bold text-orange-400" : ""}
+                        href={item.path}
+                        // onClick={handleMenuItemClick}
+                      >
+                        {item.label}
+                      </Link>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </div>
+    )
+}
