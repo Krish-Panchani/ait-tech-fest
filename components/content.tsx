d6f6e183be57dfbb06cb9c82115b82f34eafdce7
Krish-Panchani <krishpanchani1346@gmail.com>
2024-01-28 12:10:17 +0530

    Add id attribute to schedule section

--- components/content.tsx
+++ components/content.tsx
@@ -27,7 +27,7 @@ export default function Component() {
         <div className="flex flex-col min-h-[100vh]">
             <main className="flex-1">
                 <Welcome />
-                <section className="w-full py-12 md:py-24 lg:py-32">
+                <section id="schedule" className="w-full py-12 md:py-24 lg:py-32">
                     <div className="container space-y-12 px-4 md:px-6">
                         <div className="flex flex-col items-center justify-center space-y-4 text-center">
                             <div className="space-y-2">
