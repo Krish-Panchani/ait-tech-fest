585f1659abc1b350eb10533950679329f216c5a0
Krish-Panchani <krishpanchani1346@gmail.com>
2024-01-28 16:13:15 +0530

    Update Logo component

--- app/assets/Logo.tsx
+++ app/assets/Logo.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 export const Logo = () => (
-  <svg fill="none" height="36" viewBox="0 0 32 32" width="36">
-    <path
-      clipRule="evenodd"
-      d="M17.6482 10.1305L15.8785 7.02583L7.02979 22.5499H10.5278L17.6482 10.1305ZM19.8798 14.0457L18.11 17.1983L19.394 19.4511H16.8453L15.1056 22.5499H24.7272L19.8798 14.0457Z"
-      fill="currentColor"
-      fillRule="evenodd"
-    />
-  </svg>
+  <svg width="43" height="17" viewBox="0 0 43 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M6.162 0.701835L0 17H6.63L9.672 8.18807L6.162 0.701835Z" fill="currentColor"/>
+<path d="M15.132 17L7.254 0H13.728L21.606 17H15.132Z" fill="currentColor"/>
+<path d="M18.926 4.13303L17.132 0H23.372L25.4 4.13303H18.926Z" fill="currentColor"/>
+<path d="M24.776 17L19.238 5.22477H25.79L31.484 17H24.776Z" fill="currentColor"/>
+<path d="M28.57 4.13303L26.776 0H37.384L39.334 4.13303H28.57Z" fill="currentColor"/>
+<path d="M36.37 17L30.988 5.22477H37.384L43 17H36.37Z" fill="currentColor"/>
+</svg> 
 );
