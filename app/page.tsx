cc370420f643e54e423e73a91c099871e3960af6
Krish-Panchani <krishpanchani1346@gmail.com>
2024-01-28 13:04:55 +0530

    Update padding in main container

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ import Menu from "@/components/menu";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between p-8">
       <div>
         <Content />
       </div>
