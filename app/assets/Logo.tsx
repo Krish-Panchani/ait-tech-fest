c5033ad6cb625999732639f9aba4c90f6d0a48a9
Krish-Panchani <krishpanchani1346@gmail.com>
2024-02-13 21:17:22 +0530

    Update Logo component

--- app/assets/Logo.tsx
+++ app/assets/Logo.tsx
@@ -1,11 +1,52 @@
 import React from "react";
 export const Logo = () => (
-  <svg width="43" height="17" viewBox="0 0 43 17" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M6.162 0.701835L0 17H6.63L9.672 8.18807L6.162 0.701835Z" fill="currentColor"/>
-<path d="M15.132 17L7.254 0H13.728L21.606 17H15.132Z" fill="currentColor"/>
-<path d="M18.926 4.13303L17.132 0H23.372L25.4 4.13303H18.926Z" fill="currentColor"/>
-<path d="M24.776 17L19.238 5.22477H25.79L31.484 17H24.776Z" fill="currentColor"/>
-<path d="M28.57 4.13303L26.776 0H37.384L39.334 4.13303H28.57Z" fill="currentColor"/>
-<path d="M36.37 17L30.988 5.22477H37.384L43 17H36.37Z" fill="currentColor"/>
-</svg> 
+//   <svg width="43" height="17" viewBox="0 0 43 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+// <path d="M6.162 0.701835L0 17H6.63L9.672 8.18807L6.162 0.701835Z" fill="currentColor"/>
+// <path d="M15.132 17L7.254 0H13.728L21.606 17H15.132Z" fill="currentColor"/>
+// <path d="M18.926 4.13303L17.132 0H23.372L25.4 4.13303H18.926Z" fill="currentColor"/>
+// <path d="M24.776 17L19.238 5.22477H25.79L31.484 17H24.776Z" fill="currentColor"/>
+// <path d="M28.57 4.13303L26.776 0H37.384L39.334 4.13303H28.57Z" fill="currentColor"/>
+// <path d="M36.37 17L30.988 5.22477H37.384L43 17H36.37Z" fill="currentColor"/>
+// </svg> 
+<svg width="55" height="23" viewBox="0 0 55 23" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M8.52337 1.85149L1 21.625H9.09477L12.8088 10.9341L8.52337 1.85149Z" stroke="url(#paint0_linear_126_2)"/>
+<path d="M19.4751 21.625L9.85663 1H17.7609L27.3794 21.625H19.4751Z" stroke="url(#paint1_linear_126_2)"/>
+<path d="M24.1073 6.01433L21.917 1H29.5356L32.0116 6.01433H24.1073Z" stroke="url(#paint2_linear_126_2)"/>
+<path d="M31.2498 21.625L24.4883 7.33888H32.4878L39.4398 21.625H31.2498Z" stroke="url(#paint3_linear_126_2)"/>
+<path d="M35.882 6.01433L33.6916 1H46.6433L49.0241 6.01433H35.882Z" stroke="url(#paint4_linear_126_2)"/>
+<path d="M45.4052 21.625L38.8342 7.33888H46.6433L53.5 21.625H45.4052Z" stroke="url(#paint5_linear_126_2)"/>
+<defs>
+<linearGradient id="paint0_linear_126_2" x1="27.25" y1="1" x2="27.25" y2="21.625" gradientUnits="userSpaceOnUse">
+<stop stop-color="#FF9E0B"/>
+<stop offset="0.484545" stop-color="#F24E52"/>
+<stop offset="1" stop-color="#EC4899"/>
+</linearGradient>
+<linearGradient id="paint1_linear_126_2" x1="27.25" y1="1" x2="27.25" y2="21.625" gradientUnits="userSpaceOnUse">
+<stop stop-color="#FF9E0B"/>
+<stop offset="0.484545" stop-color="#F24E52"/>
+<stop offset="1" stop-color="#EC4899"/>
+</linearGradient>
+<linearGradient id="paint2_linear_126_2" x1="27.25" y1="1" x2="27.25" y2="21.625" gradientUnits="userSpaceOnUse">
+<stop stop-color="#FF9E0B"/>
+<stop offset="0.484545" stop-color="#F24E52"/>
+<stop offset="1" stop-color="#EC4899"/>
+</linearGradient>
+<linearGradient id="paint3_linear_126_2" x1="27.25" y1="1" x2="27.25" y2="21.625" gradientUnits="userSpaceOnUse">
+<stop stop-color="#FF9E0B"/>
+<stop offset="0.484545" stop-color="#F24E52"/>
+<stop offset="1" stop-color="#EC4899"/>
+</linearGradient>
+<linearGradient id="paint4_linear_126_2" x1="27.25" y1="1" x2="27.25" y2="21.625" gradientUnits="userSpaceOnUse">
+<stop stop-color="#FF9E0B"/>
+<stop offset="0.484545" stop-color="#F24E52"/>
+<stop offset="1" stop-color="#EC4899"/>
+</linearGradient>
+<linearGradient id="paint5_linear_126_2" x1="27.25" y1="1" x2="27.25" y2="21.625" gradientUnits="userSpaceOnUse">
+<stop stop-color="#FF9E0B"/>
+<stop offset="0.484545" stop-color="#F24E52"/>
+<stop offset="1" stop-color="#EC4899"/>
+</linearGradient>
+</defs>
+</svg>
+
 );
